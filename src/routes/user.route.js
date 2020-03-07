5fa1c755e6312d86fb687970fc8d31741ed5ff9f
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2020-03-07 19:25:55 +0100

    Updated routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -11,5 +11,7 @@ router.post('/login', UserController.loginUser);
 router.post('/logout', auth, UserController.logoutUser);
 router.post('/logoutAll', auth, UserController.logoutUserAll);
 router.get('/profile', auth, UserController.getUserProfile);
+router.patch('/profile/update', auth, UserController.updateUserProfile);
+router.patch('/profile/reset_password', auth, UserController.resetPassword); // When user is logged in
 
 export default router;
