ee8ec3ffd4d902a39a07d326af9efbab51096c7e
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2020-03-23 15:03:55 +0100

    Feat: created method to handle multiple file uploads

---
+++ src/utils.js/cloudinary.js
@@ -0,0 +1,29 @@
+import cloudinary from 'cloudinary';
+
+const uploadImages = async (files) => {
+  const result = {};
+
+  try {
+    const imagePromises = files.map((file) => {
+      return new Promise((resolve, reject) => {
+        cloudinary.v2.uploader.upload(
+          file.path,
+          { use_filename: true, unique_filename: false },
+          (error, res) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(res.secure_url);
+            }
+          },
+        );
+      });
+    });
+    result.images = await Promise.all(imagePromises);
+  } catch (error) {
+    result.errors = error;
+  }
+  return result;
+};
+
+export default uploadImages;
