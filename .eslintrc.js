24df2174f8f45297d320c6fceea1acaf4633af38
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2020-03-23 15:01:49 +0100

    Updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,8 @@ module.exports = {
   rules: {
     'no-underscore-dangle': 0,
     'arrow-body-style': 0,
+    'no-new': 0,
+    'implicit-arrow-linebreak': 0,
+    'function-paren-newline': 0,
   },
 };
