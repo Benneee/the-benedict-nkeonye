126c91b3b4d381ee465ceadeb9634bfd94b5d584
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2020-04-08 07:32:54 +0100

    Feat: first set of test written for user

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,2 +1,32 @@
 import * as request from 'supertest';
 import app from '../src/app';
+import User from '../src/models/user.model';
+
+test('Should sign up a new user', async () => {
+  const response = await request(app)
+    .post('/users')
+    .send({
+      name: 'Bill',
+      email: 'bill@aol.com',
+      age: 23,
+      bio: 'ever ready',
+      password: 'Ade561.',
+    })
+    .expect(201);
+
+  // Assert that the DB was changed correctly with new data
+  const user = await User.findById(response.body.user._id);
+  expect(user).not.toBeNull();
+
+  // Assertion about the response object
+  expect(resposne.body).toMatchObject({
+    user: {
+      name: 'Bill',
+      email: 'bill@aol.com',
+      age: 23,
+    },
+    token: user.tokens[0].token,
+  });
+
+  expect(user.password).not.toBe('Ade561.');
+});
